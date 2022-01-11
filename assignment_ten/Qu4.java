c7d2504f585bf964a046d58eafe6f33138882b53
Arup Basak <65061893+arup2001-basak@users.noreply.github.com>
2022-01-11 23:30:56 +0530

    Add files via upload

---
+++ assignment_ten/Qu4.java
@@ -0,0 +1,21 @@
+package assignment_ten;
+
+public class Qu4 implements Runnable {
+
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(Thread.currentThread().getName() + " [Timed: " + (i + 1) + "]");
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread qs[] = new Thread[3];
+        for (int i = 1; i < 4; i++) {
+            qs[i - 1] = new Thread(new Qu4(), "Named Number #" + i);
+            qs[i - 1].setPriority(i);
+            qs[i - 1].start();
+        }
+    }
+
+}
+
---
+++ assignment_ten/Qu6.java
@@ -0,0 +1,32 @@
+package assignment_ten;
+
+public class Qu6 extends Thread {
+    Qu6(String name) {
+        super(name);
+    }
+
+    public void run() {
+        System.out.println(
+                "Inside run() of " + Thread.currentThread().getName() + " : " + Thread.currentThread().isAlive());
+        try {
+            sleep(2000);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Qu6 q1 = new Qu6("Custom Thread");
+        System.out.println("Pre start() " + q1.getName() + ":" + q1.isAlive());
+        q1.start();
+        System.out.println("Post start() " + q1.getName() + ":" + q1.isAlive());
+        q1.join();
+        // System.out.println("Hi");
+        System.out.println("Post join() " + q1.getName() + ":" + q1.isAlive());
+        while (q1.isAlive()) {
+            Thread.sleep(100);
+        }
+        System.out.println("Post death " + q1.getName() + ":" + q1.isAlive());
+
+    }
+}
